1942e6c4f3bced131a1c6034e8797d3b84d5bff0
peter-zhu-coder <103028347+peter-zhu-coder@users.noreply.github.com>
2023-04-11 16:58:08 +0800

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <stdlio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  int a, b;
+  a = 0; 
+  b = 1;
+  cout << "hello world" << endl;
+  return 0;
+}
